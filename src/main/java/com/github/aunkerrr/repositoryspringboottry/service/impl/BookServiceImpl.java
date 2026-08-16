16ac27ddb204f4a84486fa35d60ad96733721029
Nikita Myroniuk <nikitamix1sstt@gmail.com>
2026-08-16 13:33:29 +0200

    small changes in master/main branch

--- src/main/java/com/github/aunkerrr/repositoryspringboottry/service/impl/BookServiceImpl.java
+++ src/main/java/com/github/aunkerrr/repositoryspringboottry/service/impl/BookServiceImpl.java
@@ -4,14 +4,15 @@ import com.github.aunkerrr.repositoryspringboottry.model.Book;
 import com.github.aunkerrr.repositoryspringboottry.repository.impl.BookRepositoryImpl;
 import com.github.aunkerrr.repositoryspringboottry.service.BookService;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class BookServiceImpl implements BookService {
 
-    @Autowired
-    private BookRepositoryImpl bookRepository;
+    private final BookRepositoryImpl bookRepository;
 
     @Override
     public Book save(Book entity) {
