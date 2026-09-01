0b00ad62fb5bc114c10d476915b6f62e23488090
Nikita Myroniuk <nikitamix1sstt@gmail.com>
2026-09-01 18:57:15 +0200

    feat: soft delete implemedted

--- src/main/java/com/github/aunkerrr/repositoryspringboottry/model/Book.java
+++ src/main/java/com/github/aunkerrr/repositoryspringboottry/model/Book.java
@@ -9,11 +9,15 @@ import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "books")
+@SQLDelete(sql = "UPDATE books SET isDeleted = true WHERE id=?")
+@SQLRestriction("isDeleted = false")
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,4 +32,6 @@ public class Book {
     private BigDecimal price;
     private String description;
     private String coverImage;
+    @Column(nullable = false)
+    private boolean isDeleted = false;
 }
