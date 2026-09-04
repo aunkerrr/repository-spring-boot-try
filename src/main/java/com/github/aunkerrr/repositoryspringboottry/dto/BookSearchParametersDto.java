736474bdd23c5441311335a84429f8b2dbd07b80
Nikita Myroniuk <nikitamix1sstt@gmail.com>
2026-09-04 13:52:58 +0200

    mvn checkstyle fixed

--- src/main/java/com/github/aunkerrr/repositoryspringboottry/dto/BookSearchParametersDto.java
+++ src/main/java/com/github/aunkerrr/repositoryspringboottry/dto/BookSearchParametersDto.java
@@ -1,7 +1,5 @@
 package com.github.aunkerrr.repositoryspringboottry.dto;
 
-import org.hibernate.dialect.function.SybaseTruncFunction;
-
 public record BookSearchParametersDto(
         String[] titles,
         String[] authors
