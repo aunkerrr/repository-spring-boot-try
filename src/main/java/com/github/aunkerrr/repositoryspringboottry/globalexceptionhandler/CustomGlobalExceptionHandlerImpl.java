ad32f6d3ed618fb815004d7eb6711e8b6bb69d20
Nikita Myroniuk <nikitamix1sstt@gmail.com>
2026-09-25 13:48:23 +0200

    feat: EntityNotFoundException validation added

--- src/main/java/com/github/aunkerrr/repositoryspringboottry/globalexceptionhandler/CustomGlobalExceptionHandlerImpl.java
+++ src/main/java/com/github/aunkerrr/repositoryspringboottry/globalexceptionhandler/CustomGlobalExceptionHandlerImpl.java
@@ -1,5 +1,6 @@
 package com.github.aunkerrr.repositoryspringboottry.globalexceptionhandler;
 
+import com.github.aunkerrr.repositoryspringboottry.exception.EntityNotFoundException;
 import java.time.LocalDateTime;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -13,11 +14,15 @@ import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class CustomGlobalExceptionHandlerImpl extends ResponseEntityExceptionHandler {
+
+
+
     @Override
     protected @Nullable ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
                                                                             HttpHeaders headers,
@@ -43,5 +48,16 @@ public class CustomGlobalExceptionHandlerImpl extends ResponseEntityExceptionHan
             return field + " " + message;
         }
         return error.getDefaultMessage();
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Object> handleEntityNotFoundException(EntityNotFoundException exception) {
+        Map<String, Object> body = new LinkedHashMap<>();
+
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", HttpStatus.NOT_FOUND);
+        body.put("error", exception.getMessage());
+
+        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
     }
 }
